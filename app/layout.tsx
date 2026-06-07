b23acc85d563b93f812d930d1fea4acffbbdadd5
GMconsultoria <guilhermemobtp@gmail.com>
2026-06-07 16:37:37 -0300

    Update site favicon and logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       { url: "/favicon.ico", sizes: "any" },
-      { url: "/favicon.svg", type: "image/svg+xml" },
+      { url: "/favicon.png", type: "image/png" },
     ],
     shortcut: "/favicon.ico",
     apple: "/favicon.ico",
